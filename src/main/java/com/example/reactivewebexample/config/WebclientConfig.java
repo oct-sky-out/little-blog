344ccd967e23029f6bf46fae667e138e3cf3bfe6
oct-sky-out <kms3335k@naver.com>
2022-12-01 18:16:22 +0900

    웹클라이언트 기본 세팅 빈 생성

---
+++ src/main/java/com/example/reactivewebexample/config/WebclientConfig.java
@@ -0,0 +1,33 @@
+package com.example.reactivewebexample.config;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class WebclientConfig {
+    @Value("#{${user.baseurl}}")
+    private String baseurl;
+
+    @Value("#{${user.port}}")
+    private int port;
+
+    @Bean
+    public WebClient webClient() {
+        int timeoutLimit = 10;
+
+        HttpClient httpClient = HttpClient.create()
+            .responseTimeout(Duration.of(timeoutLimit, ChronoUnit.SECONDS))
+            .baseUrl(baseurl)
+            .port(port);
+
+        return WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .build();
+    }
+}
